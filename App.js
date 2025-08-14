f4783e21eec1598599f28fbf77f927436093a379
avneetkour0403 <avneet@myanatomy.in>
2025-08-15 00:21:58 +0530

    Lesson1

---
+++ App.js
@@ -0,0 +1,16 @@
+// const heading = React.createElement( "h1", {id: "heading", xyz:"abc"}, "Hellow world from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// console.log(heading);
+
+
+// to create a nested react element
+const parent = React.createElement( "div", {id: "parent"},
+     React.createElement( "div", {id: "child"},  [
+        React.createElement( "h1", {}, "Hye, I am a H1 tag"), 
+        React.createElement( "h2", {}, "Hye, I am a H2 tag")
+    ]
+     ) 
+    );
+root.render(parent);    
