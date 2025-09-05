5affb3bc2e84ec7fc217676d2eb527f44ee48ee1
avneetkour0403 <avneet@myanatomy.in>
2025-09-05 23:38:01 +0530

    Fetching the live data from swiggy api\

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ResData from "../Utils/mockData";
 import Restaurant from "./Restaurant";
 
 const Body = () => {
-  const [filteredList, setFilteredList] = useState(ResData);
+  const [filteredList, setFilteredList] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+
+  const fetchData = async () =>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const json = await data.json();
+    console.log(json);
+    setFilteredList(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log("filteredList",filteredList);
+  }
+
   const filterRes = () => {
-    const filteredList = ResData.filter(
-      (res) => res.info.avgRating > 4
+    const newFilteredList = filteredList.filter(
+      (res) => res.info.avgRating > 4.5
     );
-    setFilteredList(filteredList);
+    setFilteredList(newFilteredList);
     console.log(filteredList);
   }
+  console.log("filteredList2",filteredList);
   return (
     <>
       <div>
